b76aeaf48668cbf55f373d854582976eb85745cc
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-03-24 00:12:51 +0000

    adding getTeam endpoint

--- Back-end/DotaNerf/DotaNerf/Controllers/TeamController.cs
+++ Back-end/DotaNerf/DotaNerf/Controllers/TeamController.cs
@@ -18,8 +18,25 @@ public class TeamController : ControllerBase
     [HttpGet(Name = "GetTeams")]
     public async Task<IActionResult> GetTeamsAsync()
     {
-        var teams = await _context.Teams.ToListAsync();
+        var teams = await _context.Teams
+            .Include(t => t.Players)
+            .ToListAsync();
 
         return Ok(teams);
+    }
+
+    [HttpGet("{id}", Name = "GetTeam")]
+    public async Task<IActionResult> GetTeamAsync(Guid id)
+    {
+        var team = await _context.Teams
+            .Include(t => t.Players)
+            .FirstOrDefaultAsync(team => team.Id == id);
+
+        if (team is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(team);
     }
 }
