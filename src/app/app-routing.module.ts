8944315930e63e041945699d9d6d329bddd5d08c
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-04-17 19:15:10 +0100

    navigate to player games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateGameFromTableComponent } from './create-game-from-table/create-game-from-table.component';
-import { CreateGameComponent } from './create-game/create-game.component';
 import { GameDisplayComponent } from './game-display/game-display.component';
-import { ImageUploadComponent } from './image-upload/image-upload.component';
-import { TableComponent } from './player-stats-table/player-stats-table.component';
+import { PlayerGamesComponent } from './player-games/player-games.component';
+import { PlayersComponent } from './player-stats-table/player-stats-table.component';
 
 const routes: Routes = [
-  { path: 'table', component: TableComponent },
-  { path: 'create-game', component: CreateGameComponent },
+  // { path: 'create-game', component: CreateGameComponent },
+  // { path: 'image-upload', component: ImageUploadComponent },
+  { path: 'players', component: PlayersComponent },
+  { path: 'players/:id', component: PlayerGamesComponent },
   { path: 'games', component: GameDisplayComponent },
-  { path: 'image-upload', component: ImageUploadComponent },
   { path: 'game', component: CreateGameFromTableComponent },
-  { path: '**', redirectTo: 'table', pathMatch: 'full' },
+  { path: '**', redirectTo: 'players', pathMatch: 'full' },
 ];
 
 @NgModule({
