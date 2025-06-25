969aa8447453954a5dab2607dce3565deed8ee67
Boko <Boko@DESKTOP-CCQH80O>
2025-06-26 00:04:48 +0100

    login page to user service

--- src/app/shared/login-page/login-page.component.ts
+++ src/app/shared/login-page/login-page.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.userService.login(email, password).subscribe({
+        next: (user) => {
+          console.log('Login successful:', user);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        },
+      });
+    }
+  }
+}
